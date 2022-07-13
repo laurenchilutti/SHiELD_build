05bf692c2903d6512a9e466e672386123dc9ebe3
Lauren Chilutti <lauren.chilutti@noaa.gov>
2022-07-13 15:17:07 -0400

    updating scripts to include --mpi-pmi2 in srun argument for parallelworks

--- site/environment.intel.sh
+++ site/environment.intel.sh
@@ -181,6 +181,7 @@ elif [ `hostname | cut -c1-4` = "mgmt" ] ; then
    export TEMPLATE=site/intel.mk
    export LAUNCHER=srun
    export AVX_LEVEL=-xhost
+   export CI=--mpi=pmi2
 
    echo -e ' '
    module list
